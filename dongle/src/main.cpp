21a1c77361ee1bfc5418e1a5d72ae705ebf7cce1
Alex <Oscarcille@gmail.com>
2025-03-04 13:14:01 +0100

    Removed empty lines, moved magic numbers into constexpr

--- dongle/src/main.cpp
+++ dongle/src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 
-char command[255];
-
-char macAddressData[18];
-
-char data[239];
+constexpr int INPUT_LENGTH = 255; 
+constexpr int MAC_ADDRESS_LENGTH = 17;
+constexpr int DATA_LENGTH = 238;
+char input[INPUT_LENGTH +1];
+char macAddressData[MAC_ADDRESS_LENGTH +1];
+char data[DATA_LENGTH +1];
 
 void setup()
 {
@@ -17,12 +18,12 @@ void parseMacAddressAndData(){
   int avaliableBytes = Serial.available();
   if (avaliableBytes > 0){
     for (int i=0; i<avaliableBytes; i++){
-      command[i] = Serial.read();
+      input[i] = Serial.read();
     }
-    strncpy(macAddressData, command, 17);
+    strncpy(macAddressData, input, MAC_ADDRESS_LENGTH);
     Serial.print("MacAddress:");
     Serial.println(macAddressData);
-    strncpy(data, command+17, 238);
+    strncpy(data, input+MAC_ADDRESS_LENGTH, DATA_LENGTH);
     Serial.print("Data:");
     Serial.println(data);
   }
